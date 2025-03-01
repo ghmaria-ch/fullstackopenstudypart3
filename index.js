55d56962c502b1408d204dd316244315a6cee976
maria christo <maria.phone.2020@gmail.com>
2025-03-01 19:25:35 +0530

    ex3.7

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const morgan = require('morgan')
 const app = express()
 
 let persons = [
@@ -25,6 +26,7 @@ let persons = [
 ]
 
 app.use(express.json())
+app.use(morgan('tiny'))
 
 const generateId = () => {
     // Generate a random number between 1000000000 and 9999999999 (a 10-digit number)
