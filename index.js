95bec65e0524b66e207d844c1d620659d06dc734
maria christo <maria.phone.2020@gmail.com>
2025-07-01 14:03:17 +0530

    3.19

--- index.js
+++ index.js
@@ -23,28 +23,7 @@ const unknownEndpoint = (request, response) => {
 };
 
 
-// let persons = [
-//     { 
-//         "id": "1",
-//         "name": "Arto Hellas", 
-//         "number": "040-123456"
-//     },
-//     { 
-//       "id": "2",
-//       "name": "Ada Lovelace", 
-//       "number": "39-44-5323523"
-//     },
-//     { 
-//       "id": "3",
-//       "name": "Dan Abramov", 
-//       "number": "12-43-234345"
-//     },
-//     { 
-//         "id": "4",
-//         "name": "Mary Poppendieck", 
-//         "number": "39-23-6423122"
-//     }
-// ]
+
 
 app.use(cors())
 app.use(express.json())
@@ -54,41 +33,7 @@ morgan.token('body', (req) => JSON.stringify(req.body)); // Convert body to a st
 // Use Morgan middleware for logging, including the request body for POST requests
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
-  // app.post('/api/persons', (request, response,next) => {
-  //   const body = request.body
-  
-  //   if (!body.name) {
-  //     return response.status(400).json({ 
-  //       error: 'name missing' 
-  //     })
-  //   }
 
-  //   if (!body.number) {
-  //       return response.status(400).json({ 
-  //         error: 'number missing' 
-  //       })
-  //     }
-      
-  //   Person.findOne({ name: body.name })
-  // .then(existingPerson => {
-  //   if (existingPerson) {
-  //     return response.status(400).json({ error: 'name must be unique' });
-  //   }
-
-      
-  //   const person = new Person({
-  //     name: body.name,
-  //     number: body.number,
-  //   });
-
-  
-  //    person
-  //   .save()
-  //   .then((savedPerson) => {
-  //     response.json(savedPerson);
-  //   })
-  //   .catch((error) => next(error)); 
-  // })
   app.post('/api/persons', (request, response, next) => {
   const { name, number } = request.body;
 
