8529a9cf7257efc64f10a7602883bff137761e97
maria christo <maria.phone.2020@gmail.com>
2025-07-01 13:25:38 +0530

    3.15&3.16

--- index.js
+++ index.js
@@ -5,29 +5,42 @@ const cors = require('cors')
 const Person = require('./models/person')
 
 const app = express()
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
 
-let persons = [
-    { 
-        "id": "1",
-        "name": "Arto Hellas", 
-        "number": "040-123456"
-    },
-    { 
-      "id": "2",
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
-    },
-    { 
-      "id": "3",
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
-    },
-    { 
-        "id": "4",
-        "name": "Mary Poppendieck", 
-        "number": "39-23-6423122"
-    }
-]
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  } 
+
+  next(error)
+}
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: "unknown endpoint" });
+};
+
+
+// let persons = [
+//     { 
+//         "id": "1",
+//         "name": "Arto Hellas", 
+//         "number": "040-123456"
+//     },
+//     { 
+//       "id": "2",
+//       "name": "Ada Lovelace", 
+//       "number": "39-44-5323523"
+//     },
+//     { 
+//       "id": "3",
+//       "name": "Dan Abramov", 
+//       "number": "12-43-234345"
+//     },
+//     { 
+//         "id": "4",
+//         "name": "Mary Poppendieck", 
+//         "number": "39-23-6423122"
+//     }
+// ]
 
 app.use(cors())
 app.use(express.json())
@@ -88,20 +101,32 @@ app.get("/info", (request, response) => {
       if (person){
       response.json(person)
       }
+       else {
+        response.status(404).end() 
+      }
     })
-    .catch(error => {
-      console.log(error)
-      response.status(500).end()
-    })
-  })
+   .catch(error => next(error))
+})
+  
 
-  app.delete('/api/persons/:id', (request, response) => {
-    const id = request.params.id
-    persons = persons.filter(person => person.id !== id)
-    response.status(204).end()
-  })
-  
-  
+  // app.delete('/api/persons/:id', (request, response) => {
+  //   const id = request.params.id
+  //   persons = persons.filter(person => person.id !== id)
+  //   response.status(204).end()
+  // })
+
+
+app.delete("/api/persons/:id", (request, response, next) => {
+  Person.findByIdAndDelete(request.params.id)
+    .then(() => {
+      response.status(204).end();
+    })
+    .catch((error) => next(error));
+});
+
+
+  app.use(unknownEndpoint);
+  app.use(errorHandler)
   const PORT = process.env.PORT
   app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
