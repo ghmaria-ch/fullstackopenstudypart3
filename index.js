104a2c013cb2d39f7f25437518735321982d9ad3
maria christo <maria.phone.2020@gmail.com>
2025-07-01 13:52:44 +0530

    3.17&3.18

--- index.js
+++ index.js
@@ -10,7 +10,11 @@ const errorHandler = (error, request, response, next) => {
 
   if (error.name === 'CastError') {
     return response.status(400).send({ error: 'malformatted id' })
-  } 
+   } 
+   else if (error.name === "ValidationError") {
+    return response.status(400).json({ error: error.message });
+  }
+ 
 
   next(error)
 }
@@ -50,37 +54,98 @@ morgan.token('body', (req) => JSON.stringify(req.body)); // Convert body to a st
 // Use Morgan middleware for logging, including the request body for POST requests
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
-  app.post('/api/persons', (request, response) => {
-    const body = request.body
+  // app.post('/api/persons', (request, response,next) => {
+  //   const body = request.body
   
-    if (!body.name) {
-      return response.status(400).json({ 
-        error: 'name missing' 
-      })
-    }
+  //   if (!body.name) {
+  //     return response.status(400).json({ 
+  //       error: 'name missing' 
+  //     })
+  //   }
 
-    if (!body.number) {
-        return response.status(400).json({ 
-          error: 'number missing' 
-        })
+  //   if (!body.number) {
+  //       return response.status(400).json({ 
+  //         error: 'number missing' 
+  //       })
+  //     }
+      
+  //   Person.findOne({ name: body.name })
+  // .then(existingPerson => {
+  //   if (existingPerson) {
+  //     return response.status(400).json({ error: 'name must be unique' });
+  //   }
+
+      
+  //   const person = new Person({
+  //     name: body.name,
+  //     number: body.number,
+  //   });
+
+  
+  //    person
+  //   .save()
+  //   .then((savedPerson) => {
+  //     response.json(savedPerson);
+  //   })
+  //   .catch((error) => next(error)); 
+  // })
+  app.post('/api/persons', (request, response, next) => {
+  const { name, number } = request.body;
+
+  if (!name) {
+    return response.status(400).json({ error: 'name missing' });
+  }
+
+  if (!number) {
+    return response.status(400).json({ error: 'number missing' });
+  }
+
+  Person.findOne({ name })
+    .then(existingPerson => {
+      if (existingPerson) {
+        return response.status(400).json({ error: 'name must be unique' });
       }
-      
-    const nameExists = persons.some(person => person.name === body.name);
-    if (nameExists) {
-        return response.status(400).json({
-            error: 'name must be unique'
-        });
-    }
-      
-    const person = new Person({
-      name: body.name,
-      number: body.number,
-    });
 
-    person.save().then(savedPerson => {
-      response.json(savedPerson)
-    })  
+      const person = new Person({ name, number });
+
+      person.save()
+        .then(savedPerson => {
+          response.json(savedPerson);
+        })
+        .catch(error => next(error));
+    })
+    .catch(error => next(error));
+});
+
+
+  app.put("/api/persons/:id", (request, response, next) => {
+  const body = request.body;
+
+  if (!body.name || !body.number) {
+    return response.status(400).json({
+      error: "name or number missing",
+    });
+  }
+
+  const person = {
+    name: body.name,
+    number: body.number,
+  };
+
+  Person.findByIdAndUpdate(request.params.id, person, {
+    new: true,
+    runValidators: true,
+    context: "query",
   })
+    .then((updatedPerson) => {
+      if (updatedPerson) {
+        response.json(updatedPerson);
+      } else {
+        response.status(404).end();
+      }
+    })
+    .catch((error) => next(error));
+});
 
 app.get('/api/persons', (request, response) => {
   Person.find({}).then(persons => {
@@ -88,13 +153,22 @@ app.get('/api/persons', (request, response) => {
   })
 })  
 
+// app.get("/info", (request, response) => {
+//     const currentDate = new Date();
+//     response.send(`
+//       <p>Phonebook has info for ${persons.length} people</p>
+//       <p>${currentDate}</p>
+//       `);
+//   });
 app.get("/info", (request, response) => {
+  Person.countDocuments({}).then((count) => {
     const currentDate = new Date();
     response.send(`
-      <p>Phonebook has info for ${persons.length} people</p>
-      <p>${currentDate}</p>
+        <p>Phonebook has info for ${count} people</p>
+        <p>${currentDate}</p>
       `);
   });
+});
 
   app.get('/api/persons/:id', (request, response) => {
     Person.findById(request.params.id).then(person => {
